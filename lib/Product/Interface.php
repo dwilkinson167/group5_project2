a545ac629dd3f7d8d4da5f2318018927fee4f58b
Anthony <stenbergdigeronimo@gmail.com>
2017-02-07 14:59:37 -0800

    Added Fonts and CSS
    Here I added the font folder and then the CSS to make the font appear on the page.

---
+++ lib/Product/Interface.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Any product implementing the Product_Interface can be handled by Cart
+
+ */
+interface Product_Interface
+{
+	public function getCost();
+	
+	public function setCost($cost = NULL);
+	
+	public function getDescription();
+	
+	public function setDescription($description = NULL);
+}
